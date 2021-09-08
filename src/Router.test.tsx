51a3f998846e64c4600ce6d0c13ec7a2d3d5751a
asnewman <ashznewman@gmail.com>
2021-09-08 16:50:47 -0700

    first commit

---
+++ src/Router.test.tsx
@@ -0,0 +1,47 @@
+import {comparePath, Failure, Success} from "./Router";
+
+describe('comparePath tests', () => {
+  it('works on valid match and slugs', () => {
+    const res = comparePath("/rally/:id", "/rally/123");
+
+    if (res instanceof Failure) {
+      throw 'Failure on valid path'
+    }
+
+    expect(res).toBeInstanceOf(Success);
+    expect(res.data).toEqual({doesMatch: true, slugs: { id: "123" }})
+  })
+
+  it('works on valid match and slugs v2', () => {
+    const res = comparePath("/rally/:id/:author/message/:messageid/day", "/rally/123/ash/message/999/day");
+
+    if (res instanceof Failure) {
+      throw 'Failure on valid path'
+    }
+
+    expect(res).toBeInstanceOf(Success);
+    expect(res.data).toEqual({doesMatch: true, slugs: { id: "123", author: "ash", messageid: "999" }})
+  })
+
+  it('success with no match on a non-match', () => {
+    const res = comparePath("/swapi/hello", "/rally/123/ash/message/999/day");
+
+    if (res instanceof Failure) {
+      throw 'Failure on valid path'
+    }
+
+    expect(res).toBeInstanceOf(Success);
+    expect(res.data).toEqual({doesMatch: false, slugs: {}})
+  })
+
+  it('success with no match on a non-match', () => {
+    const res = comparePath("/swapi/hello", "/rally/123/ash/message/999/day");
+
+    if (res instanceof Failure) {
+      throw 'Failure on valid path'
+    }
+
+    expect(res).toBeInstanceOf(Success);
+    expect(res.data).toEqual({doesMatch: false, slugs: {}})
+  })
+})
---
+++ src/Router.tsx
@@ -0,0 +1,72 @@
+// Custom router because I don't want to use a package
+
+import React from "react";
+
+export class Success<DataType> {
+  constructor(data: DataType) {
+    this.data = data;
+  }
+
+  data: DataType;
+}
+
+export class Failure {
+  constructor(error: Error) {
+    this.error = error;
+  }
+
+  error: Error;
+}
+
+
+export type Route = {
+  url: string;
+  component: React.ReactElement;
+};
+
+const routes: Route[] = [
+  {url: "/", component: <div>Rally Home</div>},
+  {url: "/rally/:id", component: <div>Rally id: </div>},
+];
+
+type RouterProps = {
+  routes?: Route[],
+  path?: string
+}
+
+const Router = ({ routes = routes, path = window.location.path }: RouterProps) => {
+  routes.  
+}
+
+
+type ComparePathResult = {
+  doesMatch: boolean;
+  slugs: Record<string, string>;
+}
+
+export const comparePath = (declaredPath: string, urlPath: string): Success<ComparePathResult> | Failure => {
+  const declaredPathWords = declaredPath.split("/");
+  const urlPathWords = urlPath.split("/");
+
+  const slugs: Record<string, string> = {};
+
+  if (declaredPathWords.length !== urlPathWords.length) return new Success({doesMatch: false, slugs: {}});
+
+  for (let wordIdx = 0; wordIdx < declaredPathWords.length; wordIdx++) {
+    if ((declaredPathWords[wordIdx] !== urlPathWords[wordIdx]) && declaredPathWords[wordIdx].charAt(0) !== ":") {
+      return new Success({doesMatch: false, slugs: {}});
+    }
+
+    if (declaredPathWords[wordIdx].charAt(0) === ":") {
+      const slugKeyword = declaredPathWords[wordIdx].substring(1)
+
+      if (slugs[slugKeyword]) {
+        return new Failure(new Error('`Error parsing url, multiple same slug keywords found: ${declaredPathWords}`'));
+      }
+
+      slugs[slugKeyword] = urlPathWords[wordIdx];
+    }
+  }
+
+  return new Success({doesMatch: true, slugs});
+}
